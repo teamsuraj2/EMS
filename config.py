90fa9941e71034a6d2a9041133413f71debc8517
shivmish8090 <apilania55@gmail.com>
2025-05-22 09:02:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 
 import os
 DB_NAME = os.getenv("DB_NAME", "edit")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7779181877:AAHh77JwnjzJwlvqkWMlAJ_eyDAtRuUcXXA")
-MONGO_URI = os.getenv("MONGO_URL", "mongodb+srv://DnsEditGaurdian:DnsEditGaurdianBot@dnseditgaurdian.ukyou.mongodb.net/?retryWrites=true&w=majority&appName=DnsEditGaurdian")
-OWNER_ID = os.getenv("OWNER_ID", "6955568347")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URL", "")
+OWNER_ID = os.getenv("OWNER_ID", "")
 LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID", -1002440588212))
